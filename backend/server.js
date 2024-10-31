461a85ddfd297f84101525008ea8fbffa7656457
swayam-creator <swayamallewar7@gmail.com>
2024-10-31 20:50:10 +0530

    Database Connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose"
 dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Connected DB");
+    
+}).catch(err=>console.log(err)
+)
 const app=express();
 const PORT = process.env.PORT || 3000;
 app.get(
